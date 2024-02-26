097690c58175c9a9b2e58b1375f7aa00cc564077
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-02-25 22:07:03 -0300

    init project save my wallet

---
+++ src/SaveMyWalletApp.tsx
@@ -0,0 +1,8 @@
+export const SaveMyWalletApp = () => {
+  return (
+    <>
+      <h1>SaveMyWalletApp v2</h1>
+      <hr />
+    </>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { SaveMyWalletApp } from './SaveMyWalletApp.tsx';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SaveMyWalletApp />
+  </React.StrictMode>
+);
