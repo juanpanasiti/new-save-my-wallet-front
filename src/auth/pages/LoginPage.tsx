01fd229bacf70845749b59898f9df57f98a1582c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-02-26 21:09:43 -0300

    install and implement react-hook-form

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -2,40 +2,63 @@ import { Login } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from 'react-hook-form';
+import { LoginForm } from '../types';
 
 export const LoginPage = () => {
-  return (
+  const { register, handleSubmit, formState } = useForm<LoginForm>();
 
-      
-        <AuthLayout title='Login'>
-          <form>
-            <Grid container>
-              {/* Fields */}
-              <Grid item xs={12} sx={{ mb: 2 }}>
-                <TextField label='Username' type='text' placeholder='username' fullWidth />
-              </Grid>
-              <Grid item xs={12} sx={{ mb: 2 }}>
-                <TextField label='Password' type='password' placeholder='******' fullWidth />
-              </Grid>
-              {/* Buttons */}
-              <Grid container spacing={2} sx={{ mb: 2 }}>
-                <Grid item xs={12}>
-                  <Button variant='contained' fullWidth>
-                    <Login />
-                    <Typography sx={{ ml: 1 }}>Login</Typography>
-                  </Button>
-                </Grid>
-              </Grid>
-              {/* Register Link */}
-              <Grid container direction='row'>
-                <Grid item xs={12}>
-                  <Typography>
-                    If you don't have an account, <Link component={RouterLink} color='inherit' to='/auth/signup'>register here</Link>
-                  </Typography>
-                </Grid>
-              </Grid>
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
+  return (
+    <AuthLayout title='Login'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container>
+          {/* Fields */}
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField
+              label='Username'
+              type='text'
+              placeholder='username'
+              fullWidth
+              {...register('username', { required: true })}
+            />
+            {formState.errors.username && <span>Something is wrong with your username</span>}
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='******'
+              fullWidth
+              {...register('password', { required: true })}
+            />
+            {formState.errors.password && <span>This field is required</span>}
+          </Grid>
+          {/* Buttons */}
+          <Grid container spacing={2} sx={{ mb: 2 }}>
+            <Grid item xs={12}>
+              <Button type='submit' variant='contained' fullWidth>
+                <Login />
+                <Typography sx={{ ml: 1 }}>Login</Typography>
+              </Button>
             </Grid>
-          </form>
-        </AuthLayout>
+          </Grid>
+          {/* Register Link */}
+          <Grid container direction='row'>
+            <Grid item xs={12}>
+              <Typography>
+                If you don't have an account,{' '}
+                <Link component={RouterLink} color='inherit' to='/auth/signup'>
+                  register here
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -2,26 +2,62 @@ import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { Login } from '@mui/icons-material';
 import { Link as RouterLink } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { RegisterForm } from '../types';
 
 export const RegisterPage = () => {
+  const { formState, register, handleSubmit } = useForm<RegisterForm>();
+  const onSubmit = (data: RegisterForm) => {
+    console.log(data);
+  };
   return (
     <AuthLayout title='Register'>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container>
           {/* Fields */}
           <Grid item xs={12} sx={{ mb: 2 }}>
-            <TextField label='Username' type='text' placeholder='username' fullWidth />
+            <TextField
+              label='Username'
+              type='text'
+              placeholder='username'
+              fullWidth
+              {...register('username', {
+                required: true,
+                minLength: 8,
+                maxLength: 20,
+                // pattern: /^(?![0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/,
+              })}
+            />
+            {formState.errors.username && <span>Something is wrong with your username</span>}
           </Grid>
           <Grid item xs={12} sx={{ mb: 2 }}>
-            <TextField label='Email' type='email' placeholder='user@mail.com' fullWidth />
+            <TextField
+              label='Email'
+              type='email'
+              placeholder='user@mail.com'
+              fullWidth
+              {...register('email', {
+                required: true,
+                maxLength: 50,
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              })}
+            />
+            {formState.errors.email && <span>Something is wrong with your email</span>}
           </Grid>
           <Grid item xs={12} sx={{ mb: 2 }}>
-            <TextField label='Password' type='password' placeholder='******' fullWidth />
+            <TextField
+              label='Password'
+              type='password'
+              placeholder='******'
+              fullWidth
+              {...register('password', { required: true })}
+            />
+            {formState.errors.password && <span>This field is required</span>}
           </Grid>
           {/* Buttons */}
           <Grid container spacing={2} sx={{ mb: 2 }}>
             <Grid item xs={12}>
-              <Button variant='contained' fullWidth>
+              <Button variant='contained' type='submit' fullWidth>
                 <Login />
                 <Typography sx={{ ml: 1 }}>Create Account</Typography>
               </Button>
