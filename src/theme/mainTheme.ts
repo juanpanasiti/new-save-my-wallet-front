26675eccc8c78368d8de5c37a8e04a030c1d2384
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-02-27 00:44:10 -0300

    change main theme as dark

--- src/theme/mainTheme.ts
+++ src/theme/mainTheme.ts
@@ -1,3 +1,54 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
 
-export const mainTheme = createTheme()
+export const mainTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#212121',
+      light: '#484848',
+      dark: '#000000',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#f44336',
+      light: '#ff7961',
+      dark: '#ba000d',
+      contrastText: '#ffffff',
+    },
+    background: {
+      paper: '#303030',
+      default: '#212121',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#c7c7c7',
+    },
+    error: {
+      main: '#f44336',
+      light: '#ff7961',
+      dark: '#ba000d',
+      contrastText: '#ffffff',
+    },
+    warning: {
+      main: '#ffeb3b',
+      light: '#fff9c4',
+      dark: '#f9a825',
+      contrastText: '#000000',
+    },
+    success: {
+      main: '#4caf50',
+      light: '#81c784',
+      dark: '#388e3c',
+      contrastText: '#ffffff',
+    },
+    info: {
+      main: '#2196f3',
+      light: '#64b5f6',
+      dark: '#0b72c4',
+      contrastText: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: ['Roboto', 'sans-serif'].join(','),
+  },
+});
