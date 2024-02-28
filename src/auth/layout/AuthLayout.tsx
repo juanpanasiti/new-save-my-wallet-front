ce41ce866acee43ab70c02a15695a77adbe07ec2
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-02-27 21:56:31 -0300

    fix auth layout colors

--- src/auth/layout/AuthLayout.tsx
+++ src/auth/layout/AuthLayout.tsx
@@ -15,11 +15,10 @@ export const AuthLayout = ({ children, title }: Props) => {
       justifyContent='center'
       sx={{
         minHeight: '100vh',
-        backgroundColor: 'black',
         padding: 4,
       }}
     >
-      <Grid item xs={3} sx={{ width: { md: 450 }, backgroundColor: 'lightgray', padding: 3, borderRadius: 2 }}>
+      <Grid item xs={3} sx={{ width: { md: 450 }, backgroundColor: 'primary.light', padding: 3, borderRadius: 2 }}>
         <Typography variant='h5' sx={{ mb: 1 }}>
           {title}
         </Typography>
