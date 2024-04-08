3187fc10cebe85aaf80bae661d476ea86cbd7a27
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-07 23:47:47 -0300

    implement credit card list api call

---
+++ src/smw/api/credit-cards.api.ts
@@ -0,0 +1,16 @@
+import smwApiClient from '../../api/smwClient';
+import { ApiEndpoints } from '../../common/enums';
+import { ApiResponse, CreditCard } from '../interfaces';
+
+export const apiCreditCardList = async (limit: number = 10, offset: number = 0, onlyMain: boolean = false): Promise<CreditCard[]> => {
+	const params = {
+		limit,
+		offset,
+        onlyMain: onlyMain ? true : null,
+	};
+
+	const { data } = await smwApiClient.get<ApiResponse<CreditCard[]>>(ApiEndpoints.CREDIT_CARDS, { params });
+	console.log(data)
+
+	return data.data;
+};
