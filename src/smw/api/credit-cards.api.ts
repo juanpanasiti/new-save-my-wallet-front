352ca82ea266234217d8cf4643f93157b84c8f46
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-12 04:19:42 -0300

    implements update and create api calls

--- src/smw/api/credit-cards.api.ts
+++ src/smw/api/credit-cards.api.ts
@@ -1,15 +1,38 @@
 import smwApiClient from '../../api/smwClient';
 import { ApiEndpoints } from '../../common/enums';
+import { cleanCreditCard } from '../helpers/clean-credit-card';
 import { ApiResponse, CreditCard } from '../interfaces';
 
-export const apiCreditCardList = async (limit: number = 10, offset: number = 0, onlyMain: boolean = false): Promise<CreditCard[]> => {
+export const apiCreditCardList = async (
+	limit: number = 10,
+	offset: number = 0,
+	onlyMain: boolean = false
+): Promise<CreditCard[]> => {
 	const params = {
 		limit,
 		offset,
-        onlyMain: onlyMain ? true : null,
+		onlyMain: onlyMain ? true : null,
 	};
 
 	const { data } = await smwApiClient.get<ApiResponse<CreditCard[]>>(ApiEndpoints.CREDIT_CARDS, { params });
+	const response = data.data.map(cc => {
+		return {
+			...cc,
+			nextClosingDate: cc.nextClosingDate && cc.nextClosingDate.slice(0,10),
+			nextExpiringDate: cc.nextExpiringDate && cc.nextExpiringDate.slice(0,10)
+		}
+	})
+	return response;
+};
+
+export const apiNewCreditCard = async (newCreditCard: CreditCard): Promise<CreditCard> => {
+	const { data } = await smwApiClient.post<ApiResponse<CreditCard>>(ApiEndpoints.CREDIT_CARDS, cleanCreditCard(newCreditCard));
+
+	return data.data;
+};
+
+export const apiUpdateCreditCard = async (creditCard: CreditCard): Promise<CreditCard> => {
+	const { data } = await smwApiClient.patch<ApiResponse<CreditCard>>(`${ApiEndpoints.CREDIT_CARDS}/${creditCard.id}`, cleanCreditCard(creditCard));
 
 	return data.data;
 };
