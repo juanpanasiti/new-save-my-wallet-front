f88e33a409d3a4e6651d215a245af7861a92326b
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-13 12:30:57 -0300

    avoid send dates and if the credit card is an extension

--- src/smw/helpers/clean-credit-card.ts
+++ src/smw/helpers/clean-credit-card.ts
@@ -1,10 +1,11 @@
 import { CreditCard } from '../interfaces';
 
-export const cleanCreditCard = ({ name, limit, ...restData }: CreditCard): Partial<CreditCard> => {
-	const response: Partial<CreditCard> = { name, limit };
-    if (restData.mainCreditCard) response.mainCreditCard = restData.mainCreditCard
-    if (restData.nextClosingDate) response.nextClosingDate = restData.nextClosingDate
-    if (restData.nextExpiringDate) response.nextExpiringDate = restData.nextExpiringDate
+export const cleanCreditCard = ({ name, limit, mainCreditCard, ...restData }: CreditCard): Partial<CreditCard> => {
+	const response: Partial<CreditCard> = { name };
+    if (mainCreditCard) response.mainCreditCard = mainCreditCard
+    if (mainCreditCard === '' && limit) response.limit = limit
+    if (mainCreditCard === '' && restData.nextClosingDate) response.nextClosingDate = restData.nextClosingDate
+    if (mainCreditCard === '' && restData.nextExpiringDate) response.nextExpiringDate = restData.nextExpiringDate
 
 	return response;
 };
