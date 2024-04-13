2c6e072747967ce15413608f1f01d07d82a97104
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-13 12:32:34 -0300

    remove unused imports and comment example code

--- src/smw/layout/SmwLayout.tsx
+++ src/smw/layout/SmwLayout.tsx
@@ -1,34 +1,34 @@
-import { Box, Button, Toolbar } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import { Navbar, Sidebar } from '../components';
 import { useState } from 'react';
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const drawerWidth = 240;
 
 export const SmwLayout = ({ children }: Props) => {
-  const [showSidebar, setShowSidebar] = useState(true);
-  const handleToggle = () => {
-    setShowSidebar(!showSidebar);
-  };
-  return (
-    <Box sx={{ display: 'flex', height: '100vh' }}>
-      <Navbar drawerWidth={drawerWidth} isDrawerOpen={showSidebar} />
-      <Sidebar drawerWidth={drawerWidth} drawerOpen={showSidebar} />
+	const [showSidebar] = useState(true);
+	// const handleToggle = () => {
+	// 	setShowSidebar(!showSidebar);
+	// };
+	return (
+		<Box sx={{ display: 'flex', height: '100vh' }}>
+			<Navbar drawerWidth={drawerWidth} isDrawerOpen={showSidebar} />
+			<Sidebar drawerWidth={drawerWidth} drawerOpen={showSidebar} />
 
-      <Box
-        component='main'
-        sx={{
-          flexGrow: 1,
-          p: 3,
-        }}
-      >
-        <Toolbar />
-        {/* <Button onClick={handleToggle}>Toggle Sidebar to {showSidebar ? 'OFF' : 'ON'}</Button> */}
-        {children}
-      </Box>
-    </Box>
-  );
+			<Box
+				component='main'
+				sx={{
+					flexGrow: 1,
+					p: 3,
+				}}
+			>
+				<Toolbar />
+				{/* <Button onClick={handleToggle}>Toggle Sidebar to {showSidebar ? 'OFF' : 'ON'}</Button> */}
+				{children}
+			</Box>
+		</Box>
+	);
 };
