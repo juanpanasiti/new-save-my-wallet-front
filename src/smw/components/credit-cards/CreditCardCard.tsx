e6cf72507213de3d0c12eb9b8a3de50d676ac483
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-19 23:13:54 -0300

    add button for delete credit card

--- src/smw/components/credit-cards/CreditCardCard.tsx
+++ src/smw/components/credit-cards/CreditCardCard.tsx
@@ -1,18 +1,23 @@
 import { Box, Button, Paper, Typography } from '@mui/material';
 import { CreditCard } from '../../interfaces';
 import { parseCurrency, parseDate } from '../../helpers';
-import { Edit } from '@mui/icons-material';
-import { useModal } from '../../hooks';
+import { DeleteForever, Edit } from '@mui/icons-material';
+import { useCreditCards, useModal } from '../../hooks';
 import { CreditCardModal } from './CreditCardModal';
 interface Props {
 	creditCard: CreditCard;
 }
 
 export const CreditCardCard = ({ creditCard }: Props) => {
+	const {deleteMutation} = useCreditCards()
 	const limitStr = parseCurrency(creditCard.limit);
 	const closesAt = parseDate(creditCard.nextClosingDate);
 	const expiringAt = parseDate(creditCard.nextExpiringDate);
 	const { open, handleOpen } = useModal();
+
+	const handleDelete = () => {
+		deleteMutation.mutate(creditCard.id)
+	}
 	return (
 		<Box component={Paper} sx={{ p: 2 }}>
 			<Typography color={'primary'} variant='h4'>
@@ -26,6 +31,9 @@ export const CreditCardCard = ({ creditCard }: Props) => {
 			<Box sx={{}}>
 				<Button onClick={handleOpen} variant='outlined'>
 					<Edit />
+				</Button>
+				<Button onClick={handleDelete} variant='outlined' color='error' sx={{ml:2}}>
+					<DeleteForever />
 				</Button>
 			</Box>
 			<CreditCardModal creditCard={creditCard} open={open} handleOpen={() => handleOpen()} />
