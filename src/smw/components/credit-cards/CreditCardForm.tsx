b0023fa5f7ddbb64c4707aee5a7ec88cc490a3e8
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-04-26 20:15:45 -0300

    fix code style

--- src/smw/components/credit-cards/CreditCardForm.tsx
+++ src/smw/components/credit-cards/CreditCardForm.tsx
@@ -4,46 +4,46 @@ import { useCreditCards } from '../../hooks';
 import { useForm } from 'react-hook-form';
 
 interface Props {
-	creditCard: CreditCard;
-	afterSubmit?: () => void;
+    creditCard: CreditCard;
+    afterSubmit?: () => void;
 }
 
 export const CreditCardForm = ({ creditCard, afterSubmit }: Props) => {
-	const { register, handleSubmit, setValue } = useForm<CreditCard>({ defaultValues: creditCard });
+    const { register, handleSubmit, setValue } = useForm<CreditCard>({ defaultValues: creditCard });
 
-	const isNew = creditCard.id === '';
-	const { mainCreditCards, createMutation, updateMutation } = useCreditCards();
+    const isNew = creditCard.id === '';
+    const { mainCreditCards, createMutation, updateMutation } = useCreditCards();
 
-	const onSubmit = (data: CreditCard) => {
-		try {
-			if (data.id === '') {
-				createMutation.mutate(data);
-			} else {
-				updateMutation.mutate(data);
-			}
-			afterSubmit && afterSubmit();
-		} catch (error) {
-			console.error(error);
-		}
-	};
-	const handleSelectChange = ({ target }: SelectChangeEvent) => {
-		setValue('mainCreditCard', target.value);
-		const selected = mainCreditCards?.find((cc) => cc.id === target.value);
-		if (selected === undefined) return;
-		setValue('limit', selected.limit)
-		setValue('nextClosingDate', selected.nextClosingDate)
-		setValue('nextExpiringDate', selected.nextExpiringDate)
-	};
-	if (mainCreditCards === undefined) return <div>Loading...</div>;
+    const onSubmit = (data: CreditCard) => {
+        try {
+            if (data.id === '') {
+                createMutation.mutate(data);
+            } else {
+                updateMutation.mutate(data);
+            }
+            afterSubmit && afterSubmit();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const handleSelectChange = ({ target }: SelectChangeEvent) => {
+        setValue('mainCreditCard', target.value);
+        const selected = mainCreditCards?.find((cc) => cc.id === target.value);
+        if (selected === undefined) return;
+        setValue('limit', selected.limit);
+        setValue('nextClosingDate', selected.nextClosingDate);
+        setValue('nextExpiringDate', selected.nextExpiringDate);
+    };
+    if (mainCreditCards === undefined) return <div>Loading...</div>;
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Box>
-				{/* mainCreditCard */}
-				<TextField
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Box>
+                {/* mainCreditCard */}
+                <TextField
                     select
                     label='Credit Card'
-					sx={{ mb: 2 }}
+                    sx={{ mb: 2 }}
                     defaultValue={creditCard.mainCreditCard || undefined}
                     fullWidth
                     onChange={(e) => handleSelectChange(e as SelectChangeEvent)}
@@ -53,42 +53,31 @@ export const CreditCardForm = ({ creditCard, afterSubmit }: Props) => {
                             {cc.name}
                         </MenuItem>
                     ))}
-					<MenuItem>None</MenuItem>
+                    <MenuItem>None</MenuItem>
                 </TextField>
-				{/* name */}
-				<TextField
-					label='Name'
-					type='text'
-					placeholder='Ej: VISA - ####'
-					fullWidth
-					sx={{ mb: 2 }}
-					{...register('name', { required: true })}
-				/>
+                {/* name */}
+                <TextField
+                    label='Name'
+                    type='text'
+                    placeholder='Ej: VISA - ####'
+                    fullWidth
+                    sx={{ mb: 2 }}
+                    {...register('name', { required: true })}
+                />
 
-				{/* limit */}
-				<TextField
-					label='Buy Limit'
-					type='number'
-					fullWidth
-					sx={{ mb: 2 }}
-					{...register('limit', { required: true, valueAsNumber: true })}
-				/>
+                {/* limit */}
+                <TextField label='Buy Limit' type='number' fullWidth sx={{ mb: 2 }} {...register('limit', { required: true, valueAsNumber: true })} />
 
-				{/* nextClosingDate */}
-				<TextField
-					type='date'
-					fullWidth
-					{...register('nextClosingDate', { required: true, min: Date() })}
-					sx={{ mb: 2 }}
-				/>
+                {/* nextClosingDate */}
+                <TextField type='date' fullWidth {...register('nextClosingDate', { required: true, min: Date() })} sx={{ mb: 2 }} />
 
-				{/* nextExpiringDate */}
-				<TextField type='date' fullWidth {...register('nextExpiringDate', { required: true })} sx={{ mb: 2 }} />
+                {/* nextExpiringDate */}
+                <TextField type='date' fullWidth {...register('nextExpiringDate', { required: true })} sx={{ mb: 2 }} />
 
-				<Button type='submit' color='primary' variant='outlined'>
-					{isNew ? 'Create' : 'Save'}
-				</Button>
-			</Box>
-		</form>
-	);
+                <Button type='submit' color='primary' variant='outlined'>
+                    {isNew ? 'Create' : 'Save'}
+                </Button>
+            </Box>
+        </form>
+    );
 };
